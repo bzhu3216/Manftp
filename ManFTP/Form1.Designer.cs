25e903ee8ad1eb6192040690566e8e05014edf05
bzhu3216 <39339504+bzhu3216@users.noreply.github.com>
2018-06-02 12:44:37 +0800

    开始

--- ManFTP/Form1.Designer.cs
+++ ManFTP/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(146, 27);
+            this.button1.Location = new System.Drawing.Point(164, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 23);
             this.button1.TabIndex = 0;
