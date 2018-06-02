2b5359081d9b826363c33953256f1d5b7c666b3b
bzhu3216 <39339504+bzhu3216@users.noreply.github.com>
2018-06-02 12:42:03 +0800

    test

--- ManFTP/Form1.Designer.cs
+++ ManFTP/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(151, 26);
+            this.button1.Location = new System.Drawing.Point(146, 27);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(126, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "选择处理的文件夹";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
